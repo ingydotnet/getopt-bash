57f7a7a8467b2bdf107c61a919d6c7b38fbd17c0
Ingy döt Net <ingy@ingy.net>
2021-01-30 14:32:27 -0500

    wip

--- lib/getopt.bash
+++ lib/getopt.bash
@@ -1,27 +1,25 @@
 # getopt-long library based around `git rev-parse --parseopt`
 
-# shellcheck shell=bash
-
 getopt() {
   local opt_spec
-  opt_spec="$(
-    echo "$GETOPT_SPEC" |
+  opt_spec=$(
+    echo "$getopt_spec" |
       grep -A999999 '^--$' |
       grep -v '^\s*$' |
       tail -n+2
-  )"
+  )
 
-  if ${GETOPT_DEFAULT_HELP:-true}; then
+  if ${getopt_default_help:-true}; then
     if [[ $# -eq 0 ]];then
       set -- --help
     fi
   fi
 
   local parsed
-  parsed="$(
-    echo "$GETOPT_SPEC" |
-    git rev-parse --parseopt -- "$@"
-  )" || true
+  parsed=$(
+    echo "$getopt_spec" |
+      git rev-parse --parseopt -- "$@"
+  ) || true
 
   if [[ $parsed =~ ^cat ]]; then
     eval "$parsed" | getopt-pager
@@ -32,14 +30,14 @@ getopt() {
 
   while IFS= read -r line; do
     if [[ $line =~ ^([a-zA-Z]+)(,([a-z]+))?(=?)\  ]]; then
-      opt_var="option_${BASH_REMATCH[1]}"
+      opt_var=option_${BASH_REMATCH[1]}
       if [[ ${BASH_REMATCH[3]} ]]; then
-        opt_var="option_${BASH_REMATCH[3]}"
+        opt_var=option_${BASH_REMATCH[3]}
       fi
       if [[ -z ${BASH_REMATCH[4]} ]]; then
         printf -v "$opt_var" false
       else
-        lines_var="${opt_var}_lines"
+        lines_var=${opt_var}_lines
         if [[ ${!lines_var-} ]]; then
           eval "$opt_var+=()"
         fi
@@ -47,8 +45,8 @@ getopt() {
     fi
   done <<<"$opt_spec"
 
-  while [ $# -gt 0 ]; do
-    local option="$1"; shift
+  while [[ $# -gt 0 ]]; do
+    local option=$1; shift
 
     [[ $option != -- ]] || break
 
@@ -57,25 +55,25 @@ getopt() {
       local wants_value=false match opt_var
       if [[ $line =~ ^([-a-zA-Z]+)(,([-a-z]+))?(=?)\  ]]; then
         if [[ ${#BASH_REMATCH[1]} -gt 1 ]]; then
-          match="--${BASH_REMATCH[1]}"
+          match=--${BASH_REMATCH[1]}
         else
-          match="-${BASH_REMATCH[1]}"
+          match=-${BASH_REMATCH[1]}
         fi
-        opt_var="option_${BASH_REMATCH[1]}"
+        opt_var=option_${BASH_REMATCH[1]}
         if [[ ${BASH_REMATCH[3]} ]]; then
-          opt_var="option_${BASH_REMATCH[3]}"
+          opt_var=option_${BASH_REMATCH[3]}
         fi
         opt_var=${opt_var//-/_}
         if [[ ${BASH_REMATCH[4]} ]]; then
           wants_value=true
         fi
       else
-        die "Invalid GETOPT_SPEC option line: '$line'"
+        die "Invalid getopt_spec option line: '$line'"
       fi
 
       if [[ $option == "$match" ]]; then
         if $wants_value; then
-          lines_var="${opt_var}_lines"
+          lines_var=${opt_var}_lines
           if [[ ${!lines_var-} ]]; then
             eval "$opt_var+=('$1')"
           else
@@ -94,15 +92,15 @@ getopt() {
   done
 
   local i arg_name arg_var required=false array=false re1='^\+'
-  for arg_name in $GETOPT_ARGS; do
-    arg_var="${arg_name//-/_}"
+  for arg_name in $getopt_args; do
+    arg_var=${arg_name//-/_}
     if [[ $arg_var =~ ^@ ]]; then
       array=true
-      arg_var="${arg_var#@}"
+      arg_var=${arg_var#@}
     fi
     if [[ $arg_var =~ $re1 ]]; then
       required=true
-      arg_var="${arg_var#+}"
+      arg_var=${arg_var#+}
     fi
 
     if [[ $# -gt 0 ]]; then
@@ -120,7 +118,7 @@ getopt() {
       die "'$arg_name' is required"
     fi
   done
-  [ $# -eq 0 ] || die "Unexpected arguments: '$*'"
+  [[ $# -eq 0 ]] || die "Unexpected arguments: '$*'"
 }
 
 getopt-pager() {
